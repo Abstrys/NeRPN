53298dbd83cd41dbbf87580da496c4422023c515
Eron Hennessey <eron@abstrys.com>
2019-03-09 18:25:37 -0800

    fixup build an linux install script

---
+++ shell/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo Installing NeRPN...
+if [ -d ${HOME}/bin ]; then
+    cp NeRPN.jar ${HOME}/bin
+    echo "java -jar NeRPN.jar &" >${HOME}/bin/nerpn
+    chmod +x ${HOME}/bin/nerpn
+    echo Done--you can run \'nerpn\' to start the program!
+else
+   echo You have no ${HOME}/bin directory!
+fi
+
